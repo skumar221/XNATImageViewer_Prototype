8e8bcd380ba782fcb317f5120bbdbdf918c4c8c6
Sunil Kumar <kumars@mir.wustl.edu>
2013-06-04 10:59:40 -0500

    Mass rename

---
+++ scripts/utils/dom/validateArgs.js
@@ -0,0 +1,23 @@
+//******************************************************
+//  Determines if there's an argument match between template and args
+//  with template being the priority
+//******************************************************
+utils.dom.validateArgs = function (originString, templateArgs, args, callback) {
+	
+	var errorStr = "A " + originString + " has invalid arguments:";
+	
+    for (var attr in args) { 
+    	if (! (attr in templateArgs)) {
+    		throw (errorStr + " '" + attr + "' "); 
+    	}
+    	else{
+    		
+    		// Recurse if the value is an object
+   			if (args[attr].toString() === '[object Object]') {
+   				utils.dom.validateArgs(originString, templateArgs[attr], args[attr])
+   			} 		
+    	}
+    }
+
+	if (callback) { callback() };
+}
