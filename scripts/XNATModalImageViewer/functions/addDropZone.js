95f633adff2d789cb3e5b31105271e172b776886
Sunil Kumar <sunil.kumar@alumni.duke.edu>
2013-06-03 22:19:47 -0500

    Hosted refactor

--- scripts/XNATModalImageViewer/functions/addDropZone.js
+++
@@ -1,9 +0,0 @@
-XNATModalImageViewer.prototype.addDropZone = function (dz) {
-	
-	//----------------------------------
-	//	SET DROPZONES
-	//----------------------------------			
-	for (var i=0; i < this.ScrollGallery.thumbs.length; i++) {
-		this.ScrollGallery.thumbs[i].addDropZone(dz);	
-	}
-}
