8cc1b1916355a61cd3b24bcdd63232393aa2c4cf
Sunil Kumar <sunil.kumar@alumni.duke.edu>
2013-07-09 11:50:31 -0700

    Successfully compiled utils

--- python/scriptsBackup_2013-07-09__11_07_37/utils/convert/pct.js
+++
@@ -1,7 +0,0 @@
-goog.provide('utils.convert.pct')
-
-utils.convert.pct = function (value) {
-	return (value * 100).toString() + "%";
-}
-
-goog.exportSymbol('utils.convert.pct', utils.convert.pct);	 
